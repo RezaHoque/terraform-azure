1b0e33d819994db77c7fd42362943c6727642a30
Rezaul Hoque <Rezaul.Hoque@velux.com>
2023-11-23 12:13:19 +0100

    added app service plan

--- main.tf
+++ main.tf
@@ -15,8 +15,24 @@ provider "azurerm" {
 
 }
 
+# Generate a random integer to create a globally unique name
+#resource "random_integer" "ri" {
+ # min = 10000
+  #max = 99999
+#}
+
+#creating resource group
 resource "azurerm_resource_group" "rg" {
   name     = "rg-terraform"
   location = "northeurope"
 
+}
+
+#creating linus app service plan
+resource "azurerm_service_plan" "appserviceplan" {
+  name = "app-asp-${azurerm_resource_group.rg.name}"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type = "Linux"
+  sku_name = "B1"
 }
