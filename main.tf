fe7e08401561adf05a78cfb01d06211a02f2f7e4
Rezaul Hoque <rezaul.hoque@velux.com>
2023-11-25 19:08:56 +0100

    acr

--- main.tf
+++ main.tf
@@ -72,3 +72,17 @@ resource "azurerm_app_service" "appservice" {
     type = "SystemAssigned"
   }
 }
+
+#creating azure container registry
+resource "azurerm_container_registry" "acr" {
+  name = "acrToDoRgTerraform"
+  resource_group_name = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.rg.location
+  sku = "Basic"
+  admin_enabled = true
+  tags = {
+    "environment" = "dev"
+    "source"      = "Terraform"
+  }
+  
+}
